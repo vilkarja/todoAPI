7a1eff1e12d10752979d6e26bb83061836949a91
Ville Karjalainen <ville.karjalainen@student.tut.fi>
2017-09-16 10:05:45 +0300

    Set up separate test and dev dbs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoAPP',{useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 
 module.exports = {
     mongoose
 };
+
