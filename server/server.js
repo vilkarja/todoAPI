d9b52176a1332ac988279acaf13be1571322eefc
Ville Karjalainen <ville.karjalainen@student.tut.fi>
2017-09-16 09:05:58 +0300

    Add patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-//lenovo test push
+
 var app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
@@ -63,6 +64,28 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     }); 
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text'], ['completed']);
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(404).send();
+    });
 });
 
 app.listen(port, () => {
